05d5c80b6c7ada561c64e3e049eca30bab7f512e
DanyBittan <danielbittang@gmail.com>
2025-08-17 19:33:54 +0200

    feat: Icon implementation

--- src/app/components/Projects/ProjectsShowcase.jsx
+++ src/app/components/Projects/ProjectsShowcase.jsx
@@ -1,5 +1,6 @@
 import projectData from "@/data/project-data.json";
 import Image from "next/image";
+import InfoIcon from "../Icons/InfoIcon";
 
 export default function ProjectShowcase(prop) {
   const project = projectData[prop.projectName];
@@ -11,12 +12,11 @@ export default function ProjectShowcase(prop) {
         alt="Dizmanga picture"
         fill
         priority
-        sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
         className={`object-cover object-center ${prop.imgStyle} transition-all duration-200 absolute z-0`}
       />)}
       <div className="w-full h-20 bottom-0 bg-purple-950 border-t-2 border-purple-600 absolute z-10 rounded-b-lg flex justify-center items-center text-4xl font-alien">
         <h1>{prop.projectName}</h1>
-        <button className="w-8 h-auto bg-gray-200 absolute left-4">aa</button>
+        <InfoIcon className="w-10 h-6 absolute left-4 invert">aa</InfoIcon>
       </div>
     </div >
   );
