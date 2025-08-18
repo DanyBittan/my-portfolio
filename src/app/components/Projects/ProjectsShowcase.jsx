e1c5ce53d93718bfffa031788f8772ad10b08f5f
DanyBittan <danielbittang@gmail.com>
2025-08-18 17:29:44 +0200

    feat: add state management for project showcase interaction

--- src/app/components/Projects/ProjectsShowcase.jsx
+++ src/app/components/Projects/ProjectsShowcase.jsx
@@ -1,10 +1,12 @@
 import projectData from "@/data/project-data.json";
 import Image from "next/image";
 import InfoIcon from "../Icons/InfoIcon";
+import { useState } from "react";
 
 export default function ProjectShowcase(prop) {
   const project = projectData[prop.projectName];
-  console.log(prop)
+  const [openProject, setOpenProject] = useState(false);
+  console.log(openProject);
   return (
     <div className={`${prop.classes} text-white relative group`}>
       {project.img && (<Image
@@ -15,8 +17,8 @@ export default function ProjectShowcase(prop) {
         className={`object-cover object-center ${prop.imgStyle} transition-all duration-200 absolute z-0`}
       />)}
       <div className="w-full h-20 bottom-0 bg-purple-950 border-t-2 border-purple-600 absolute z-10 rounded-b-lg flex justify-center items-center text-4xl font-alien">
+        <InfoIcon className="w-8 h-6 fill-current text-purple-500 absolute left-4 cursor-pointer" onClick={() => setOpenProject(true)} />
         <h1>{prop.projectName}</h1>
-        <InfoIcon className="w-10 h-6 absolute left-4 invert">aa</InfoIcon>
       </div>
     </div >
   );
