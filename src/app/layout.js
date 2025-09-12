d426f0745665831a089b673c040b065e88262794
DanyBittan <danielbittang@gmail.com>
2025-09-12 20:10:15 +0200

    fix: analytics wrong import

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 import './globals.css'
 import './synthwave.css'
 import { Inter } from 'next/font/google'
-import Analytics from '@vercel/analytics/next';
+import Analytics from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ['latin'] })
 
