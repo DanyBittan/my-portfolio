5c8d84cc1adfef251bffb82022de7405da721a33
DanyBittan <danielbittang@gmail.com>
2026-04-25 13:23:37 +0200

    refactor: delete of unused commented html component

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -16,9 +16,7 @@ export default function Home() {
 
   return (
     <main>
-      {/* puedo usar el section para sustituir el div inferior */}
       <div className="synthwave-background z-0" />
-      {/* <header className="fixed w-full h-16 blur-sm bg-black/50 t-0 z-10"></header> */}
       <SynthIntro></SynthIntro>
       <About></About>
     </main>
